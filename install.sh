0a4ef6ef2fd1462c15dc4750d99784cc5d5591ad
Kostas Dichalas <kdihalas@gmail.com>
2018-12-15 12:49:00 +0000

    First commit

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+echo ":: Adding prerequisites"
+helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/ &> out.txt
+helm repo add rook-beta https://charts.rook.io/master &>> out.txt
+
+echo "::: Creating namespaces"
+kubectl create ns dashboard &>> out.txt
+kubectl create ns kube-public &>> out.txt
+kubectl create ns monitoring &>> out.txt
+kubectl create ns storage &>> out.txt
+kubectl create ns storage-ceph &>> out.txt
+kubectl create ns ingress &>> out.txt
+
+echo ":: Installing helm"
+echo ":: Creating service accounts"
+kubectl create sa tiller -n kube-system &>> out.txt
+kubectl create sa tiller -n kube-public &>> out.txt
+kubectl create sa do-admin -n kube-system &>> out.txt
+
+echo ":: Creating cluster role bindings"
+kubectl create clusterrolebinding cluster-admin-do-admin --clusterrole=cluster-admin --serviceaccount=kube-system:do-admin &>> out.txt
+kubectl create clusterrolebinding cluster-admin-system-tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller &>> out.txt
+kubectl create clusterrolebinding cluster-admin-public-tiller --clusterrole=cluster-admin --serviceaccount=kube-public:tiller &>> out.txt
+kubectl create clusterrolebinding cluster-admin-dashboard --clusterrole=cluster-admin --serviceaccount=dashboard:dashboard-kubernetes-dashboard &>> out.txt
+
+echo ":: Writing token to file .token (use this to login to dashboard)"
+kubectl get secret -n kube-system | grep do-admin | awk '{print $1}' | kubectl get secret -n kube-system -o jsonpath='{.items[0].data.token}' | base64 -d > .token
+
+echo ":: Initializing helm"
+helm init --force-upgrade --service-account tiller --tiller-namespace kube-public &>> out.txt
+helm init --force-upgrade --service-account tiller --tiller-namespace kube-system &>> out.txt
+
+echo ":: Waiting tiller to start"
+
+sleep 20;
+
+echo ":: Instaling the addons"
+echo ":: Installing nginx controller"
+helm upgrade --tiller-namespace kube-public --namespace ingress --force --install ingress stable/nginx-ingress -f config/ingress/values.yaml &>> out.txt
+
+echo ":: Installing prometheus operator and kube-prometheus"
+helm upgrade --tiller-namespace kube-public --namespace monitoring --force --install prometheus-operator coreos/prometheus-operator &>> out.txt
+helm upgrade --tiller-namespace kube-public --namespace monitoring --force --install kube-prometheus coreos/kube-prometheus -f config/monitoring/values.yaml &>> out.txt
+echo ":: To access grafana run:  kubectl port-forward -n monitoring service/kube-prometheus-grafana 8080:80 and open http://localhost:8080"
+
+echo ":: Installing kubrnetes dashboard"
+helm upgrade --tiller-namespace kube-public --namespace dashboard --force --install dashboard stable/kubernetes-dashboard -f config/dashboard/values.yaml &>> out.txt
+echo ":: To login to cluster run: kubectl port-forward -n dashboard service/dashboard-kubernetes-dashboard 8080:443 and use the provided token in .token file"
